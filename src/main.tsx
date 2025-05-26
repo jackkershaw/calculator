c828597c39dc4e467adc9d2b6c9bf71f3d709e71
jackkershaw <hello@jackkershaw.net>
2025-05-26 14:12:57 +0100

    use old render method to fix broken tests (known fcc bug)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./styles.css";
-import App from "./App.tsx";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById("root"));
