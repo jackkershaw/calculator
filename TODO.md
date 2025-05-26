965304a08928922e28cc4f38e9508fbbbf09f30e
jackkershaw <hello@jackkershaw.net>
2025-05-26 12:52:11 +0100

    fixing failing tests

--- TODO.md
+++ TODO.md
@@ -1,26 +1,5 @@
-Objective: Build an app that is functionally similar to this: https://javascript-calculator.freecodecamp.rocks/.
-
-User Story #9: In any order, I should be able to add, subtract, multiply and divide a chain of numbers of any length, and when I hit =, the correct result should be shown in the element with the id of display.
-
-User Story #10: When inputting numbers, my calculator should not allow a number to begin with multiple zeros.
-
-User Story #11: When the decimal element is clicked, a . should append to the currently displayed value; two . in one number should not be accepted.
-
-User Story #12: I should be able to perform any operation (+, -, \*, /) on numbers containing decimal points.
-
-User Story #13: If 2 or more operators are entered consecutively, the operation performed should be the last operator entered (excluding the negative (-) sign). For example, if 5 + _ 7 = is entered, the result should be 35 (i.e. 5 _ 7); if 5 _ - 5 = is entered, the result should be -25 (i.e. 5 _ (-5)).
+// fix failing tests, first this one:
 
 User Story #14: Pressing an operator immediately following = should start a new calculation that operates on the result of the previous evaluation.
 
-User Story #15: My calculator should have several decimal places of precision when it comes to rounding (note that there is no exact standard, but you should be able to handle calculations like 2 / 7 with reasonable precision to at least 4 decimal places).
-
-Note On Calculator Logic: It should be noted that there are two main schools of thought on calculator input logic: immediate execution logic and formula logic. Our example utilizes formula logic and observes order of operation precedence, immediate execution does not. Either is acceptable, but please note that depending on which you choose, your calculator may yield different results than ours for certain equations (see below example). As long as your math can be verified by another production calculator, please do not consider this a bug.
-
-EXAMPLE: 3 + 5 x 6 - 2 / 4 =
-
-    Immediate Execution Logic: 11.5
-    Formula/Expression Logic: 32.5
-
-Once you're done, submit the URL to your working project with all its tests passing.
-
-[text](https://www.freecodecamp.org/learn/front-end-development-libraries/front-end-development-libraries-projects/build-a-javascript-calculator)
+Once you're done, submit the URL to your working project with all its tests passing: (https://www.freecodecamp.org/learn/front-end-development-libraries/front-end-development-libraries-projects/build-a-javascript-calculator)
--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,61 @@ import { useState } from "react";
 function App() {
   const [input, setInput] = useState("0");
   const [output, setOutput] = useState("0");
+  const [decimalCount, setDecimalCount] = useState("0");
 
   const clearInput = () => {
-    setInput("");
+    setInput("0");
     setOutput("0");
+  };
+
+  const addNumber = (number: string) => {
+    if (input === "0") {
+      setInput(number);
+    } else {
+      setInput(input + number);
+    }
+  };
+
+  // will need to rewrite this for multiple decimal numbers // User Story #11: When the decimal element is clicked, a . should append to the currently displayed value; two . in one number should not be accepted.
+
+  const addDecimal = () => {
+    if (decimalCount == "0") {
+      setInput(input + ".");
+      setDecimalCount("1");
+    } else {
+      return;
+    }
+  };
+
+  const addZero = () => {
+    if (input === "0") {
+      return;
+    } else {
+      addNumber("0");
+    }
+  };
+
+  const addOperator = (operator: string) => {
+    if (input.endsWith(operator)) {
+      return;
+    } else {
+      setInput(input + operator);
+      setDecimalCount("0");
+    }
+  };
+
+  const calc = () => {
+    setInput(eval(input));
+    setOutput(eval(input));
+    setDecimalCount("0");
   };
 
   return (
     <div className="p-3 bg-neutral-200 min-h-screen">
       <h1 className="text-8xl font-bold">Calculator</h1>
       <div>
-        <span
-          id="display"
-          className="bg-neutral-900 text-white text-4xl font-bold p-10 my-5 rounded-lg w-full h-30 block flex flex-col justify-center text-right"
-        >
-          <span id="input" className="text-gray-300">
+        <span className="bg-neutral-900 text-white text-4xl font-bold p-10 my-5 rounded-lg w-full h-30 block flex flex-col justify-center text-right">
+          <span id="display" className="text-gray-300">
             {input}
           </span>
           <span id="output">{output}</span>
@@ -26,55 +66,55 @@ function App() {
       <div className="grid-cols-3 grid gap-5">
         <button
           id="zero"
-          onClick={() => setInput(input + "0")}
+          onClick={addZero}
           className="bg-blue-500 text-white text-4xl font-bold p-3 rounded-lg"
         >
           0
         </button>
         <button
           id="one"
-          onClick={() => setInput(input + "1")}
+          onClick={() => addNumber("1")}
           className="bg-blue-500 text-white text-4xl font-bold p-3 rounded-lg"
         >
           1
         </button>
         <button
           id="two"
-          onClick={() => setInput(input + "2")}
+          onClick={() => addNumber("2")}
           className="bg-blue-500 text-white text-4xl font-bold p-3 rounded-lg"
         >
           2
         </button>
         <button
           id="three"
-          onClick={() => setInput(input + "3")}
+          onClick={() => addNumber("3")}
           className="bg-blue-500 text-white text-4xl font-bold p-3 rounded-lg"
         >
           3
         </button>
         <button
           id="four"
-          onClick={() => setInput(input + "4")}
+          onClick={() => addNumber("4")}
           className="bg-blue-500 text-white text-4xl font-bold p-3 rounded-lg"
         >
           4
         </button>
         <button
           id="five"
-          onClick={() => setInput(input + "5")}
+          onClick={() => addNumber("5")}
           className="bg-blue-500 text-white text-4xl font-bold p-3 rounded-lg"
         >
           5
         </button>
         <button
-          onClick={() => setInput(input + "6")}
+          onClick={() => addNumber("6")}
           id="six"
           className="bg-blue-500 text-white text-4xl font-bold p-3 rounded-lg"
         >
           6
         </button>
         <button
-          onClick={() => setInput(input + "7")}
+          onClick={() => addNumber("7")}
           id="seven"
           className="bg-blue-500 text-white text-4xl font-bold p-3 rounded-lg"
         >
@@ -82,51 +122,56 @@ function App() {
         </button>
         <button
           id="eight"
-          onClick={() => setInput(input + "8")}
+          onClick={() => addNumber("8")}
           className="bg-blue-500 text-white text-4xl font-bold p-3 rounded-lg"
         >
           8
         </button>
         <button
           id="nine"
-          onClick={() => setInput(input + "9")}
+          onClick={() => addNumber("9")}
           className="bg-blue-500 text-white text-4xl font-bold p-3 rounded-lg"
         >
           9
         </button>
         <button
           id="decimal"
-          onClick={() => setInput(input + ".")}
+          onClick={addDecimal}
           className="bg-blue-500 text-white text-4xl font-bold p-3 rounded-lg"
         >
           .
         </button>
         <button
           id="equals"
+          onClick={calc}
           className="bg-red-500 text-white text-4xl font-bold p-3 rounded-lg"
         >
           =
         </button>
         <button
           id="add"
+          onClick={() => addOperator("+")}
           className="bg-gray-500 text-white text-4xl font-bold p-3 rounded-lg"
         >
           +
         </button>
         <button
           id="subtract"
+          onClick={() => addOperator("-")}
           className="bg-gray-500 text-white text-4xl font-bold p-3 rounded-lg"
         >
           -
         </button>
         <button
           id="multiply"
+          onClick={() => addOperator("*")}
           className="bg-gray-500 text-white text-4xl font-bold p-3 rounded-lg"
         >
           x
         </button>
         <button
           id="divide"
+          onClick={() => addOperator("/")}
           className="bg-gray-500 text-white text-4xl font-bold p-3 rounded-lg"
         >
           ÷
